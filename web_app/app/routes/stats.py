37bf4816e1b3d6f22b2d99d400ed467c8b8e5686
Anna Bardina <bardinaanny@gmail.com>
2025-05-23 18:41:07 +0300

    fix changing types of answers in cerate_survey

--- web_app/app/routes/stats.py
+++ web_app/app/routes/stats.py
@@ -8,13 +8,12 @@ bp = Blueprint('stats', __name__, url_prefix='/stats')
 survey_repository = SurveyRepository()
 
 
-# Общая статистика по всем опросам (например, сколько пройдено, активных и т.п.)
 @bp.route('/')
 @login_required
 def overall_stats():
     pass
 
-# Статистика по конкретному опросу: сколько людей прошли, распределение ответов
+
 @bp.route('/survey/<int:survey_id>')
 @login_required
 def survey_stats(survey_id):
@@ -27,20 +26,3 @@ def survey_stats(survey_id):
     return render_template("stats/survey_stats.html", survey=survey)
 
 
-# Подробная статистика по конкретному вопросу внутри опроса
-@bp.route('/question/<int:question_id>')
-@login_required
-def question_stats(question_id):
-    pass
-
-# Статистика по пользователям: кто сколько прошёл, какие результаты
-@bp.route('/users')
-@login_required
-def user_stats():
-    pass
-
-# Результаты конкретного пользователя
-@bp.route('/user/<int:user_id>')
-@login_required
-def single_user_stats(user_id):
-    pass
