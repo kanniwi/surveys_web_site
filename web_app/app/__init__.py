40f9e24ddb33cf87ced674bd7a9a00bcacf268b9
Anna Bardina <bardinaanny@gmail.com>
2025-05-26 14:50:49 +0300

    fix for pythonanywhere

--- web_app/app/__init__.py
+++ web_app/app/__init__.py
@@ -22,9 +22,6 @@ def create_app():
 
     from app.routes import main
     app.register_blueprint(main.bp)
-
-    from app.routes import question    
-    app.register_blueprint(question.bp)
     
     from app.routes import survey
     app.register_blueprint(survey.bp)
--- web_app/wsgi.py
+++ web_app/wsgi.py
@@ -3,5 +3,5 @@ from flask import render_template
 
 application = create_app()
 
-if __name__ == '__main__':
+if __name__ == '__main__':kanniwi1.pythonanywhere.com
     application.run(debug=True)
